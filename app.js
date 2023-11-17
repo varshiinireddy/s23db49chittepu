b7afbdc9e21bde44953c2f484cb7e0390701e254
Chittepu Varshini <varshinireddy963@gmail.com>
2023-11-17 11:33:05 -0600

    Commited all files

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
 var app = express();
 require('dotenv').config();
-const connectionString =process.env.MONGO_CON
+const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
 
@@ -14,8 +36,9 @@ mongoose.connect(connectionString);
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 
 
@@ -37,6 +60,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboardcat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -50,12 +83,12 @@ app.use('/instrumentss', instrumentsRouter);
 //app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -66,31 +99,34 @@ app.use(function(err, req, res, next) {
 });
 
 // We can seed the collection if needed onserver start
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything
-    await instrument.deleteMany();
+  await instrument.deleteMany();
   let instance1 = new instrument({ name: "Instrument 1", product_type: "piano", price: 399.99 });
-  instance1.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)
-  });
-  
-  let instance2 = new instrument({name: "Instrument 2", product_type: "guitar", price: 949.99});
-  instance2.save().then(doc=>{
-  console.log("secound object saved")}
-  ).catch(err=>{
-  console.error(err)
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
   });
 
-  let instance3 = new instrument({name: "Instrument 3", product_type: "harp", price: 699.99});
-  instance3.save().then(doc=>{
-  console.log("Third object saved")}
-  ).catch(err=>{
-  console.error(err)
-  });
+  let instance2 = new instrument({ name: "Instrument 2", product_type: "guitar", price: 949.99 });
+  instance2.save().then(doc => {
+    console.log("secound object saved")
   }
-  let reseed = true;
-  if (reseed) {recreateDB();}
-  
-  module.exports = app;
+  ).catch(err => {
+    console.error(err)
+  });
+
+  let instance3 = new instrument({ name: "Instrument 3", product_type: "harp", price: 699.99 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
+module.exports = app;
